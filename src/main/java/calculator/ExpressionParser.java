7e52336d0464d0f5b02c0919ffc83ffce88f6989
Vulturul2k <94383678+Vulturul2k@users.noreply.github.com>
2026-04-01 21:41:46 +0200

    bug * fixed

--- src/main/java/calculator/ExpressionParser.java
+++ src/main/java/calculator/ExpressionParser.java
@@ -7,12 +7,42 @@ import org.antlr.v4.runtime.tree.ParseTree;
 public class ExpressionParser {
 
     public static Expression parse(String expressionString) {
-        CalculatorLexer lexer = new CalculatorLexer(CharStreams.fromString(expressionString));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        CalculatorParser parser = new CalculatorParser(tokens);
-        ParseTree tree = parser.prog();
+        String input = expressionString.trim();
 
-        CalculatorVisitorImpl visitor = new CalculatorVisitorImpl();
-        return visitor.visit(tree);
+        Expression result = tryParse(input);
+
+        if (result == null && input.startsWith("(")) {
+            result = tryParse("*" + input);
+        }
+
+        if (result == null) {
+            CalculatorLexer lexer = new CalculatorLexer(CharStreams.fromString(input));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            CalculatorParser parser = new CalculatorParser(tokens);
+            ParseTree tree = parser.prog();
+            CalculatorVisitorImpl visitor = new CalculatorVisitorImpl();
+            return visitor.visit(tree);
+        }
+
+        return result;
+    }
+
+    private static Expression tryParse(String input) {
+        try {
+            CalculatorLexer lexer = new CalculatorLexer(CharStreams.fromString(input));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            CalculatorParser parser = new CalculatorParser(tokens);
+            parser.removeErrorListeners();
+            ParseTree tree = parser.prog();
+
+            if (parser.getNumberOfSyntaxErrors() > 0) {
+                return null;
+            }
+
+            CalculatorVisitorImpl visitor = new CalculatorVisitorImpl();
+            return visitor.visit(tree);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }

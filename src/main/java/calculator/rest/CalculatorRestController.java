7096fcb6a1a24ae37ce6a1e8dc69636c1689cb39
koluyantrez <210447@umons.ac.be>
2026-04-04 12:38:38 +0200

    Handle all of invalid arithmetic expression. Nothing on the terminal and throw a 400 error.

--- src/main/java/calculator/rest/CalculatorRestController.java
+++ src/main/java/calculator/rest/CalculatorRestController.java
@@ -69,8 +69,14 @@ public class CalculatorRestController {
         input = input.replace("\"", "");
         Expression e = ExpressionParser.parse(input);
         Calculator c = new Calculator();
-        int result = c.eval(e);
-        return ResponseEntity.ok(new EvaluateResponse(result));
+        try {
+            int result = c.eval(e);
+            return ResponseEntity.ok(new EvaluateResponse(result));
+        } catch (IllegalConstruction ex) {
+            return ResponseEntity.badRequest().build();
+        } catch (ArithmeticException ex) {
+            return ResponseEntity.status(400).build(); // I'm a teapot... (used here to indicate an arithmetic error like division by zero)
+        }
     }
 
     /**
